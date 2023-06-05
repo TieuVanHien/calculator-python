fadc6a26f63d6dc9da37aa588c5661e8f8c256ed
Van Hien <hiench2001@gmail.com>
2023-06-05 15:13:19 -0600

    feat: add completed task function

--- app.py
+++ app.py
@@ -3,15 +3,8 @@ import tkinter as tk
 window = tk.Tk()
 window.geometry("600x500")
 
-# Define the number of columns in the app
-num_columns = 3
-
-# Configure the columns
-for i in range(num_columns):
-    window.grid_columnconfigure(i, weight=1)
-
 # create label and entry for task name
-label_task = tk.Label(master=window, text="Task:")
+label_task = tk.Label(master=window, text="Task:", width=20)
 label_task.grid(row=0, column=0)
 
 entry_task = tk.Entry(master=window, width=50)
@@ -19,6 +12,7 @@ entry_task.grid(row=0, column=1, sticky=tk.W)
 entry_task.focus()
 
 tasks = []
+completed_tasks = []
 
 def add_task():
     task = entry_task.get()
@@ -27,16 +21,27 @@ def add_task():
         entry_task.delete(0, tk.END)  # Clear the entry widget
         update_task_frame()  # Update the task frame
 
-# Create a label frame
-frame1 = tk.LabelFrame(window, bg="white")
-frame1.grid(row=2, column=1)
+# Create a created task frame
+frame1 = tk.LabelFrame(window, bg="white", width=60)
+frame1.grid(row=2, column=0, padx=10)
+
+# Create completed task frame
+frame2 = tk.LabelFrame(window, bg="white", width=60)
+frame2.grid(row=2, column=2, padx=10)
 
 # label for created tasks
 label_created_task = tk.Label(master=window, text="Task Created")
-label_created_task.grid(row=1, column=1)
+label_created_task.grid(row=1, column=0)
 
-todo_task = tk.Label(frame1, bg="white", text="", width=40)
+todo_task = tk.Label(frame1, bg="white", text="", width=50)
 todo_task.grid(row=0, column=0)
+
+# Label for completed tasks
+label_completed = tk.Label(master=window, text="Completed Tasks")
+label_completed.grid(row=1, column=2)
+
+completed_task = tk.Label(frame2, bg="white",text="")
+completed_task.grid(row=0, column=0)
 
 # button
 button = tk.Button(
@@ -53,9 +58,12 @@ def delete_task(index):
         del tasks[index]  # Remove the task from the list 
         update_task_frame()  # Update the task frame
 
-# Configure grid for resizing
-frame1.grid_rowconfigure(0, weight=1)
-frame1.grid_columnconfigure(0, weight=1)
+# Complete task function
+def completed_taks(index):
+    task = tasks.pop(index)
+    completed_tasks.append(task)
+    update_task_frame()
+
 
 def update_task_frame():
     for widget in frame1.winfo_children():
@@ -72,8 +80,22 @@ def update_task_frame():
         complete_button = tk.Button( 
             master=frame1,
             text="Complete",
+            command=lambda index=i: completed_taks(index)
         )
         complete_button.grid(row=i, column=2, padx=5)
+    
+    for widget in frame2.winfo_children():
+        widget.destroy()
+     # Update completed tasks frame
+    for i, task in enumerate(completed_tasks):
+        task_label = tk.Label(master=frame2, text=task)
+        task_label.grid(row=i, column=0, sticky="w")
+        # delete_button = tk.Button(
+        #     master=frame2,
+        #     text="Delete",
+        #     command=lambda index=i: delete_completed_task(index)
+        # )
+        # delete_button.grid(row=i, column=1, padx=5)    
 
 
 update_task_frame()    
